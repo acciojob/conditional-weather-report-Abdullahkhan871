cf9ddbed2299e355fbb7324fd2394db80bd7721c
Abdullahkhan871 <akhan283785@gmail.com>
2025-05-20 22:30:28 +0530

    third commnet

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  const obj = {
+  const obj = useState({
     Temperature: 25, Conditions: "Sunny"
-  }
+  })
+  console.log()
   return (
     <div>
       <WeatherDisplay
-        temperature={obj.Temperature}
-        Conditions={obj.Conditions}
+        Temperature={obj[0].Temperature}
+        Conditions={obj[0].Conditions}
       />
     </div>
   )
